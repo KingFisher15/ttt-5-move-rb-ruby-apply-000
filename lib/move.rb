f9a945aa96b1cc8582bd9f120e2220533a07ba0a
crisp-delimiter-5393 <gordond227@gmail.com>
2020-07-18 17:30:09 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,7 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(spots)
-  spots = "1" - "9"
+  spots = {"1" - "9"}
   puts " '1' .to_i | '2' .to_i | '3' .to_i "
   puts "-----------"
   puts " '4' .to_i | '5'.to_i | '6' .to_i "
