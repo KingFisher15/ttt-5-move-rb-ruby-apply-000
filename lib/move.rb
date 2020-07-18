5cf8538d177f3022ee9574d57eafc76712c28497
crisp-delimiter-5393 <gordond227@gmail.com>
2020-07-18 17:14:44 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,24 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index
+  puts " 1 | 2 | 3 "
+  puts "-----------"
+  puts " 4 | 5 | 6 "
+  puts "-----------"
+  puts " 7 | 8 | 9 "
+end
+
+def move
+  puts " #{self.board_move[1]} | #{self.board_move[2]} | #{self.board_move[3]} "
+  puts "-----------"
+  puts " #{self.board_move[4]} | #{self.board_move[5]} | #{self.board_move[6]} "
+  puts "-----------"
+  puts " #{self.board_move[7]} | #{self.board_move[8]} | #{self.board_move[9]} "
+end
+
+def board_move
+  @board_move ={"1" => " ", "2" => " ", "3" => " ",
+  "4" => " ", "5" => " ","6" => " ",
+"7" => " ", "8" => " ", "9" => " "}
+end
