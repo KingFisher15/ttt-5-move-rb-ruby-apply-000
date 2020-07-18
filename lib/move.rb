4789aa155996158a2de9db5e077d3702174ad9d2
crisp-delimiter-5393 <gordond227@gmail.com>
2020-07-18 17:34:06 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(converted_input.to_i)
+def input_to_index(converted_input)
   puts " '1' .to_i | '2' .to_i | '3' .to_i "
   puts "-----------"
   puts " '4' .to_i | '5'.to_i | '6' .to_i "
