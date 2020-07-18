0c75cf3fee99463182cff8b62ea49cac2352659c
crisp-delimiter-5393 <gordond227@gmail.com>
2020-07-18 17:58:19 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,11 +8,11 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index
-  puts " "1".to_i | "2".to_i | "3".to_i "
+  puts " 1 | 2 | 3 "
   puts "-----------"
-  puts " "4".to_i | "5".to_i | "6".to_i "
+  puts " 4 | 5 | 6 "
   puts "-----------"
-  puts " "7".to_i | "8".to_i | "9".to_i "
+  puts " 7 | 8 | 9 "
 end
 
 def move
@@ -24,7 +24,7 @@ def move
 end
 
 def board_move
-  @board_move ={"1" => " ", "2" => " ", "3" => " ",
+  @board_move ={"1" => " ".to_i, "2" => " ".to_i, "3" => " ".to_i,
   "4" => " ", "5" => " ","6" => " ",
   "7" => " ", "8" => " ", "9" => " "}
 end
